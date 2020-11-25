652ebaa5764083f4379c2d43bcb47fea4c420b62
佐藤現 <halogen322@gmail.com>
2020-11-25 19:40:47 +0900

    お問い合わせページの作成

--- page-contact.php
+++ page-contact.php
@@ -5,16 +5,6 @@ Template Name: お問い合わせページ
 ?>
 <?php get_header(); ?>
 <!-- 固定ページ用のページ -->
-<main>
-      <div class="contact_header">
-        <h3>CONTACT</h3>
-        <p>
-          下記フォームにご入力下さい。<br />
-          <span class="required_color">*</span>
-          は入力必須項目になります。
-        </p>
-      </div>
-      <?php echo apply_filters('the_content', '[contact-form-7 id="151" title="mimakuru-lab"]'); ?>
-    </main>
-    <link href="<?php echo get_template_directory_uri(); ?>/css/contact/contact.css" rel="stylesheet">
+<h1>お問い合わせ</h1>
+<?php echo apply_filters('the_content', '[contact-form-7 id="151" title="mimakuru-lab"]'); ?>
 <?php get_footer(); ?>

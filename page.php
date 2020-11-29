002d6616b693e59994da241452a50a65f7093206
佐藤現 <halogen322@gmail.com>
2020-11-29 22:00:15 +0900

    page.php の追記

--- page.php
+++ page.php
@@ -5,5 +5,13 @@ Template Name:　固定ページ
 ?>
 <?php get_header(); ?>
 <!-- 固定ページ用のページ -->
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
+    <h1><?php the_title(); ?></h1>
+
+    <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('Y.m.d'); ?></time>
+    <p><?php the_content(); ?></p>
+
+<?php endwhile;
+endif; ?>
 <?php get_footer(); ?>
